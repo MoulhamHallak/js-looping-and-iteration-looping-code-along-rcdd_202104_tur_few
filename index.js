9995d9035df9b16b15446785cd361f80b47dae71
Moulham Hallak <mou990@gmail.com>
2021-04-22 21:41:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 let array = ["Ada", "Brendan", "Ali"];
-let event = "birthday";
+let event = "surprise";
 function writeCards(array, event){
   for (let i = 0; i < array.length; i++){
     console.log([`Thank you, ${array[i]}, for the wonderful ${event[0]} gift!`]);
