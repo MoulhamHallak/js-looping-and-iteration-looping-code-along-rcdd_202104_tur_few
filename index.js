07e56cb5b35d1f455f4f6cc8dd6f16e6bc62f892
Moulham Hallak <mou990@gmail.com>
2021-04-22 22:04:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let event = "surprise";
 let newArray=[];
 function writeCards(array, event){
   for (let i = 0; i < array.length; i++){
-    newArray = [`Thank you, ${array[0]}, for the wonderful ${event} gift!`, `Thank you, ${array[1]}, for the wonderful ${event} gift!`, `Thank you, ${array[2]}, for the wonderful ${event} gift!`];
+    newArray = [`Thank you, ${array[i]}, for the wonderful ${event} gift!`];
   }
-  console.log(newArray);
+  return newArray;
 }
 writeCards(array, event)
