d592516fe64cc987ec8264f076999c812931aee0
Moulham Hallak <mou990@gmail.com>
2021-04-22 21:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ function writeCards(array, event){
   for (let i = 0; i < array.length; i++){
     console.log([`Thank you, ${array[i]}, for the wonderful ${event[0]} gift!`]);
   }
+  return array;
 }
