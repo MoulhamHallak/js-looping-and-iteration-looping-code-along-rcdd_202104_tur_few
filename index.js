59d4cebface888212d2399768c44694560860272
Moulham Hallak <mou990@gmail.com>
2021-04-22 22:08:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 let array = ["Lisa", "Kaitlin", "Jan"];
-let event = "surprise';
+let event = 'surprise';
 let newArray=[];
 function writeCards(array, event){
   for (let i = 0; i < array.length; i++){
-    newArray[i] = [`Thank you, ${array[i]}, for the wonderful ${event} gift!`];
+    newArray[i] = ["Thank you," + array[i], + "for the wonderful " + event + "gift!"];
     console.log(newArray)
   }
   return newArray;
