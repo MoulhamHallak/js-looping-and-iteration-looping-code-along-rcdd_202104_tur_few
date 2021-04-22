09887503bf60cbe55e44b549cf7ad54a34432c47
Moulham Hallak <mou990@gmail.com>
2021-04-22 21:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Code your solutions in this file
 let array = ["Ada", "Brendan", "Ali"];
 let event = "surprise";
+let newArray=[];
 function writeCards(array, event){
   for (let i = 0; i < array.length; i++){
-    let newArray = [`Thank you, ${array[i]}, for the wonderful ${event} gift!`];
+    newArray = [`Thank you, ${array[i]}, for the wonderful ${event} gift!`];
   }
   return newArray;
 }
